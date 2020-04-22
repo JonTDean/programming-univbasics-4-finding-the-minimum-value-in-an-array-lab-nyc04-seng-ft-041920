f773c7a1ccc529e44f0b774848b4d664bff7064c
Jonathan T Dean <jonathantomdean@gmail.com>
2020-04-22 20:50:50 +0000

    Automatically backed up by Learn

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -1,15 +1,43 @@
 # Steps
 
-# find smallest num
-def find_min_value(array)
-    
+# Merge Sort Function
+def find_min(array)
+  minimum = merge_sort(array)[0]
 end
 
-# sort array 
-def array_sort(array)
-  sortedArray = []
-  
-  array.each do |s|
-    puts s
+# Merge Sort Function
+def merge_sort(array)
+  if array.length <= 1
+    puts array
+    array
+  else
+    mid = (array.length / 2).ceil
+
+    # Take the left half of the array
+    left_Half = merge_sort(array[0..mid - 1])
+
+    # Take the right half of the array
+    right_Half = merge_sort(array[mid..array.length])
+    
+    # Take the values from above and throw them into merge
+    merge(left_Half, right_Half)
   end
 end
+
+# Merge Function
+def merge(left_Half, right_Half)
+  # For error validation, Is one side empty?
+  if left_Half[0] == nil # if array has an unknown data set use .empty?
+    right_Half
+  elsif right_Half[0] == nil
+    left_Half
+  elsif left_Half[0] < right_Half[0]
+    # We are appending left.first to a return argument
+    [left_Half[0]] + merge(left_Half[1..left_Half.length], right_Half) # Is .first faster?
+  else
+    # Do the same for the right side
+    [right_Half[0]] + merge(left_Half, right_Half[1..right_Half.length])
+  end
+end
+
+p find_min([14, 7, 3, 12])
