509772f3304f49be0ed88d9cd5b63d1236142e26
Jonathan T Dean <jonathantomdean@gmail.com>
2020-04-22 20:50:36 +0000

    Automatically backed up by Learn

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -1,3 +1,15 @@
+# Steps
+
+# find smallest num
 def find_min_value(array)
-  # Add your solution here
+    
+end
+
+# sort array 
+def array_sort(array)
+  sortedArray = []
+  
+  array.each do |s|
+    puts s
+  end
 end
